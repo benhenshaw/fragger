ea503bf1e978249f8373dd641214c1fdfaa58dee
benhenshaw <benedicthenshaw@gmail.com>
2018-02-07 01:14:16 +0000

    Added extensive comments; added description of built in uniforms

--- ReadMe.md
+++ ReadMe.md
@@ -6,7 +6,7 @@ fragger [args] [file.glsl]
 ```
 
 | Argument | Description |
-| ---      | ---         |
+| ---      | --- |
 | **\***   | The first argument without a '-' will be treated as the shader file to read. If none is provided fragger will attempt to open the file 'frag.glsl' in the current directory. |
 | **-d**   | Print debug info. |
 | **-r**   | Retina display mode (2x scale). |
@@ -14,3 +14,11 @@ fragger [args] [file.glsl]
 Uses [SDL2](https://libsdl.org) and [glad](http://glad.dav1d.de/).
 
 An [example shader](https://github.com/benhenshaw/fragger/blob/master/creation.glsl) is provided.
+
+#### Uniforms
+
+| Name       | type  | Description |
+| ---        | ---   | --- |
+| resolution | vec2  | The width and height of the window. |
+| mouse      | vec2  | The x and y position of the mouse. |
+| time       | float | The number of seconds since the program launched. |
--- fragger.c
+++ fragger.c
@@ -9,6 +9,7 @@
 #include <SDL2/SDL_stdinc.h>
 #include "glad.c"
 
+// Exit the program, displaying an error message via pop-up box and print out.
 void panic_exit(char * message, ...) {
     va_list args;
     va_start(args, message);
@@ -21,8 +22,11 @@ void panic_exit(char * message, ...) {
 }
 
 int main(int argument_count, char ** arguments) {
+    // Disable output buffering.
+    // (This helps for some text editors, such as Sublime Text.)
     setbuf(stdout, NULL);
 
+    // Handle program arguments.
     int retina_mode = 0;
     int debug_mode = 0;
     char * frag_file_name = NULL;
@@ -38,8 +42,10 @@ int main(int argument_count, char ** arguments) {
         }
     }
 
+    // If no file was given, fall back to the default.
     if (!frag_file_name) frag_file_name = "frag.glsl";
 
+    // Print some debug info.
     if (debug_mode) {
         printf(
             "FRAGGER (Debug)\n"
@@ -50,19 +56,15 @@ int main(int argument_count, char ** arguments) {
         );
     }
 
+    // Attempt to initialise SDL2.
     int error = SDL_Init(SDL_INIT_VIDEO);
     if (error) {
         panic_exit("Could not initialise SDL2.\n%s", SDL_GetError());
     }
 
-    SDL_GL_LoadLibrary(NULL);
-    SDL_GL_SetAttribute(SDL_GL_ACCELERATED_VISUAL, 1);
-    SDL_GL_SetAttribute(SDL_GL_CONTEXT_MAJOR_VERSION, 3);
-    SDL_GL_SetAttribute(SDL_GL_CONTEXT_MINOR_VERSION, 3);
-    SDL_GL_SetAttribute(SDL_GL_CONTEXT_PROFILE_MASK, SDL_GL_CONTEXT_PROFILE_CORE);
-    SDL_GL_SetSwapInterval(1);
-
+    // Set some flags for the window.
     int window_flags = SDL_WINDOW_RESIZABLE | SDL_WINDOW_OPENGL;
+    // Only allow high-dpi if the retina flag is set.
     if (retina_mode) window_flags |= SDL_WINDOW_ALLOW_HIGHDPI;
 
     SDL_Window * window = SDL_CreateWindow("",
@@ -73,26 +75,45 @@ int main(int argument_count, char ** arguments) {
         panic_exit("Could not create window.\n%s", SDL_GetError());
     }
 
+    // Set up OpenGL context.
+    SDL_GL_LoadLibrary(NULL);
+    SDL_GL_SetAttribute(SDL_GL_ACCELERATED_VISUAL, 1);
+    SDL_GL_SetAttribute(SDL_GL_CONTEXT_MAJOR_VERSION, 3);
+    SDL_GL_SetAttribute(SDL_GL_CONTEXT_MINOR_VERSION, 3);
+    SDL_GL_SetAttribute(SDL_GL_CONTEXT_PROFILE_MASK, SDL_GL_CONTEXT_PROFILE_CORE);
+    SDL_GL_SetSwapInterval(1);
+
+    // Attempt to create the context.
     SDL_GLContext context = SDL_GL_CreateContext(window);
     if (!context) {
         panic_exit("Could not create OpenGL context.\n%s", SDL_GetError());
     }
 
+    // Dynamically load the OpenGL functions.
     gladLoadGLLoader(SDL_GL_GetProcAddress);
 
+    // Get the window dimensions.
     int width, height;
-    int scale = retina_mode ? 2 : 1;
     SDL_GetWindowSize(window, &width, &height);
+
+    // Assume 2x scale when in retina mode.
+    // (This is not very robust, but is better than nothing.)
+    int scale = retina_mode ? 2 : 1;
+
+    // Our window dimensions do not take into account retina scale, so scale them.
     width  *= scale;
     height *= scale;
 
     glViewport(0, 0, width, height);
 
+    // We do not need these features, so disable them.
     glDisable(GL_DEPTH_TEST);
     glDisable(GL_CULL_FACE);
 
+    // Clear to black.
     glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
 
+    // Print some GL context info if in debug mode.
     if (debug_mode) {
         printf("Vendor:   %s\n", glGetString(GL_VENDOR));
         printf("Renderer: %s\n", glGetString(GL_RENDERER));
@@ -100,24 +121,31 @@ int main(int argument_count, char ** arguments) {
         printf("\n");
     }
 
+    // Use the simplest possible vertex shader.
     char vert[] = "#version 330\n"
                   "in vec4 vert;\n"
                   "void main() {\n"
                   "    gl_Position = vert;\n"
                   "}\n";
 
+    // Read in the fragment shader file.
     // TODO: Read files of any length.
     char * frag = calloc(4096, 1);
+    if (!frag) panic_exit("Could not allocate space for the fragment source.");
+    // Attempt to open the file.
     FILE * frag_file = fopen(frag_file_name, "r");
     if (!frag_file) {
         panic_exit("Could not open file '%s'.", frag_file_name);
     }
+    // Attempt to read the file.
     if (!fread(frag, 1, 4096, frag_file)) {
         panic_exit("Could not read from file '%s'.", frag_file_name);
     }
 
+    // This will hold the status of the compilation.
     int status = 0;
 
+    // Attempt to compile the vertex shader.
     GLuint vertex_shader = glCreateShader(GL_VERTEX_SHADER);
     glShaderSource(vertex_shader, 1, (const char * []) { vert }, NULL);
     glCompileShader(vertex_shader);
@@ -128,6 +156,7 @@ int main(int argument_count, char ** arguments) {
         panic_exit("Vertex shader compilation failed:\n%s", message);
     }
 
+    // Attempt to compile the fragment shader.
     GLuint fragment_shader = glCreateShader(GL_FRAGMENT_SHADER);
     glShaderSource(fragment_shader, 1, (const char * []) { frag }, NULL);
     glCompileShader(fragment_shader);
@@ -138,6 +167,7 @@ int main(int argument_count, char ** arguments) {
         panic_exit("Fragment shader ('%s') compilation failed:\n%s", frag_file_name, message);
     }
 
+    // Attempt to create and link the program.
     GLuint program = glCreateProgram();
     glAttachShader(program, vertex_shader);
     glAttachShader(program, fragment_shader);
@@ -149,55 +179,62 @@ int main(int argument_count, char ** arguments) {
         panic_exit("Shader program link failed.");
     }
 
+    // Make this the active program.
     glUseProgram(program);
 
+    // Load two triangles that will cover the whole screen.
     GLuint vertex_array_object[1];
     GLuint buffers[1];
     const GLuint vertex_count = 6;
-
     glGenVertexArrays(1, vertex_array_object);
     glBindVertexArray(vertex_array_object[0]);
-
     GLfloat vertices[vertex_count][2] = {
-        { -1.0, -1.0 },
-        {  1.0, -1.0 },
-        {  1.0,  1.0 },
-        {  1.0,  1.0 },
-        { -1.0,  1.0 },
-        { -1.0, -1.0 }
+        { -1.0, -1.0 }, {  1.0, -1.0 }, {  1.0,  1.0 },
+        {  1.0,  1.0 }, { -1.0,  1.0 }, { -1.0, -1.0 }
     };
-
     glGenBuffers(1, buffers);
     glBindBuffer(GL_ARRAY_BUFFER, buffers[0]);
     glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
-
     glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, 0, 0);
     glEnableVertexAttribArray(0);
 
+    // Set the initial value of the resolution uniform to the current width and height.
     glUniform2f(glGetUniformLocation(program, "resolution"), width, height);
 
+    // Begin the frame loop.
     while (1) {
+        // Handle any queued events.
         SDL_Event event;
         while (SDL_PollEvent(&event)) {
             if (event.type == SDL_QUIT) {
                 exit(0);
             } else if (event.type == SDL_MOUSEMOTION) {
+                // Update the mouse uniform when the mouse has moved.
                 glUniform2f(glGetUniformLocation(program, "mouse"),
                     event.motion.x, height - event.motion.y);
             } else if (event.type == SDL_WINDOWEVENT) {
                 if (event.window.event == SDL_WINDOWEVENT_SIZE_CHANGED) {
-                    width = event.window.data1 * scale;
+                    // Update the resolution uniform when the window is resized.
+                    width  = event.window.data1 * scale;
                     height = event.window.data2 * scale;
                     glUniform2f(glGetUniformLocation(program, "resolution"), width, height);
+                    // Update the view port with the new resolution.
+                    glViewport(0, 0, width, height);
                 }
             }
         }
 
+        // Update the time uniform.
         glUniform1f(glGetUniformLocation(program, "time"), SDL_GetTicks() / 1000.0f);
 
+        // Clear the screen.
         glClear(GL_COLOR_BUFFER_BIT);
+
+        // Render the screen-covering triangles.
         glBindVertexArray(vertex_array_object[0]);
         glDrawArrays(GL_TRIANGLES, 0, vertex_count);
+
+        // Display the results.
         SDL_GL_SwapWindow(window);
     }
 }
